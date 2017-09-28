29e2634fdd4924acb0a0b1fb5dcf3ff0ee9e6278
dio <contact@dionysio.com>
2017-09-28 11:32:48 +0200

    removed admin

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,15 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
 from django.conf import settings
 
-#admin.site.login_template = 'registration/login.html'
-admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-
     url(r'^', include('base.urls')),
 ]
 
@@ -30,7 +25,13 @@ urlpatterns = [
 if settings.DEBUG:
     from django.conf.urls.static import static
     import debug_toolbar
+    from django.contrib import admin
 
-    urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls)),
+    # admin.site.login_template = 'registration/login.html'
+    admin.autodiscover()
+
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+        url(r'^admin/', admin.site.urls),
     ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS) + \
                    static(settings.MEDIA_URL, document_root=settings.MEDIA_DIRS)
